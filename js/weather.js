9e530344493b84a0bc2cb0c943b9eb4a81d41bab
ehdwls43 <ehdwls43@gmail.com>
2022-01-23 19:11:11 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -4,7 +4,7 @@ const API_KEY = "935d53b18e91396523206d9b25669b9c";
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then(response => response.json())
         .then(data =>{
